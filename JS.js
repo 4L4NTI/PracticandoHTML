8887dbce18300fab2a45e12feec2df981339da7c
Alan <alan108055@gmail.com>
2025-06-15 22:53:34 -0300

    modifique un poco

---
+++ JS.js
@@ -0,0 +1,54 @@
+
+const whitespace = document.getElementById("whitespace").innerHTML;
+console.log(whitespace);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let f=0
+const nom= 'hola'
+
+while (f<10){
+    if (f%2 == 0){
+        console.log('numero par ', f)
+    }
+    f++
+}
+let N=10 
+for(let a=0; a<N ;a++){
+    if (a%2 == 0){
+    console.log('numero par ', a)
+    }
+}
+let animales=['leon','chancho','tigre']
+
+for(let animal of animales){
+  console.log('animales ', animal)
+}*/
